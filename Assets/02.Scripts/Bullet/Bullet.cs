ade48f547857620c2d6d938a845fd819dc59d16a
mongilteacher <mongilteacher@gmail.com>
2026-09-03 10:29:28 +0900

    feat: pre-commit 코딩 컨벤션 검사 기능 추가

--- Assets/02.Scripts/Bullet/Bullet.cs
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -5,10 +5,11 @@ public class Bullet : MonoBehaviour
     // 목적: 총알을 위로 움직이고 싶다.
 
     public float Speed;
-    
+
+
     private void Update()
     {
-        Vector2 direction = Vector2.up;//  new Vector2(0, 1);
+        Vector2 direction = Vector2.up; //  new Vector2(0, 1);
         transform.Translate(direction * Speed * Time.deltaTime);
     }
 }
--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -6,42 +6,41 @@ public class PlayerFire : MonoBehaviour
     // 필요 속성
     // - 총알 프리팹
     public GameObject BulletPrefab;
-    
+
     // - 생성 위치(총구)
     public Transform LeftFirePoint;
     public Transform RightFirePoint;
-    
+
     // - 쿨타이머
     public float CoolTime = 0.5f;
     public float CoolTimer = 0;
-    
+
     // - 오토 모드
     public bool AutoFireMode = false;
-    
 
     private void Start()
     {
         CoolTimer = CoolTime;
     }
-    
-    
+
+
     private void Update()
     {
         // 오토 공격 모드 토글
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            AutoFireMode = !AutoFireMode; 
+            AutoFireMode = !AutoFireMode;
         }
-        
+
         // 0. 쿨타이머 감소
         CoolTimer -= Time.deltaTime;
-        
+
         // 1. 쿨타이머가 0초 이하이고 && (스페이스바를 누르거나 || 오토 모드라면)
         if (CoolTimer <= 0 && (Input.GetKeyDown(KeyCode.Space) || AutoFireMode))
         {
             // 2. 발사
             Fire();
-            
+
             // 3. 쿨타이머 초기화
             CoolTimer = CoolTime;
         }
@@ -52,9 +51,9 @@ public class PlayerFire : MonoBehaviour
         // 2. 총알 프리팹을 생성한다.
         // Instantiate는 프리팹을 복사해서 (Monobehaviour를 상속받는)게임 오브젝트를 생성하고 씬에 넣어주는 기능
         GameObject leftBullet = Instantiate(BulletPrefab);
-        leftBullet.transform.position = LeftFirePoint.position;  // 생성한 총알의 위치를 총구의 위치로
-        
+        leftBullet.transform.position = LeftFirePoint.position; // 생성한 총알의 위치를 총구의 위치로
+
         GameObject rightBullet = Instantiate(BulletPrefab);
-        rightBullet.transform.position = RightFirePoint.position;  // 생성한 총알의 위치를 총구의 위치로
+        rightBullet.transform.position = RightFirePoint.position; // 생성한 총알의 위치를 총구의 위치로
     }
 }
