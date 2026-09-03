de8b899dc30c3cf80c007c788655b5518ba2d84c
mongilteacher <mongilteacher@gmail.com>
2026-09-03 14:47:57 +0900

    feat: 적 체력과 총알 대미지를 만들어서 적 체력이 0 이하가 되면 적 삭제 (총알은 충돌시 무조건 삭제)

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float Health = 100;
-    public float MoveSpeed;
+    [SerializeField] private int _health = 100;
+    [SerializeField] private float _moveSpeed;
 
     private void Update()
     {
         Vector2 direction = Vector2.down;
 
-        transform.Translate(direction * MoveSpeed * Time.deltaTime);
+        transform.Translate(direction * _moveSpeed * Time.deltaTime);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _health -= damage;
+        if (_health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
