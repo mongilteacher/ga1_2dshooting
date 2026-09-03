1bd3b51bd5db8d39864f2977d6e04fa8cb2fe1ce
mongilteacher <mongilteacher@gmail.com>
2026-09-03 12:56:09 +0900

    feat: 충알 충돌시 나죽고 너죽자

--- Assets/02.Scripts/Bullet/Bullet.cs
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -1,15 +1,40 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     // 목적: 총알을 위로 움직이고 싶다.
-
-    public float Speed;
+    public float MoveSpeed;
 
 
     private void Update()
     {
         Vector2 direction = Vector2.up; //  new Vector2(0, 1);
-        transform.Translate(direction * Speed * Time.deltaTime);
+        transform.Translate(direction * MoveSpeed * Time.deltaTime);
+    }
+
+    // 충돌 관련 이벤트 (Enter -> Stay -> Exit)
+
+    // 충돌이 시작되면 호출되는 이벤트 함수
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("충돌 했다!");
+
+
+        // 나죽고!
+        Destroy(this.gameObject);
+
+        // 너죽자!
+        Destroy(collision.gameObject);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        //Debug.Log("충돌중이다.!");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // Debug.Log("충돌이 끝났다.!");
     }
 }
