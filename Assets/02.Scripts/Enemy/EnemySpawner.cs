e8cd35d92b4f8c7027e839d44d5b8fa8b18adac4
mongilteacher <mongilteacher@gmail.com>
2026-09-04 15:41:59 +0900

    feat: 각 스포너가 적을 스폰할때 확률에 따라 다른 타입의 적을 스폰

--- Assets/02.Scripts/Enemy/EnemySpawner.cs
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -9,8 +9,8 @@ public class EnemySpawner : MonoBehaviour
 
     private float _timer;
 
-    // - 생성할 프리팹
-    [SerializeField] private Enemy _enemyPrefab;
+    // - 생성할 프리팹들
+    [SerializeField] private Enemy[] _enemyPrefabs;
 
     private void Update()
     {
@@ -28,7 +28,32 @@ public class EnemySpawner : MonoBehaviour
 
     private void Spawn()
     {
-        Enemy enemy = Instantiate(_enemyPrefab);
+        // 각 스포너가 적을 스폰할때 확률에 따라 다른 타입의 적을 스폰해주세요.
+        // 50%: [0] Downward
+        // 30%: [1] Aimed
+        // 20%: [2] Homing
+
+        int enemyPrefabIndex = 0;
+        int radomPercent = UnityEngine.Random.Range(0, 100);
+
+        // Todo: Scriptable Object를 사용해서 리팩토링
+        // 이유 1: 배열을 사용했지만 각 아이템이 어떤 프리팹인지 알수가 없음
+        // 이유 2: 각 에너미 스폰 확률을 매직 넘버로 하드코딩해서 유지보수가 어렵
+        if (radomPercent < 50)
+        {
+            enemyPrefabIndex = 0;
+        }
+        else if (radomPercent < 80)
+        {
+            enemyPrefabIndex = 1;
+        }
+        else
+        {
+            enemyPrefabIndex = 2;
+        }
+
+
+        Enemy enemy = Instantiate(_enemyPrefabs[enemyPrefabIndex]);
         enemy.transform.position = transform.position;
     }
 }
