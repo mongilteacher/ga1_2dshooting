a3d30f16adf9085d2d03e90bc7a45e1338a72798
mongilteacher <mongilteacher@gmail.com>
2026-09-04 12:52:37 +0900

    feat: 스포너의 적 스폰 주기 스폰할때마다 랜덤

--- Assets/02.Scripts/Enemy/EnemySpawner.cs
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -5,12 +5,12 @@ public class EnemySpawner : MonoBehaviour
 {
     // 필요 속성
     // - 타이머
-    [Header("스폰 간격")][SerializeField] private float _spawnInterval = 3f;
+    [SerializeField] private float _spawnInterval = 3f;
 
     private float _timer;
 
     // - 생성할 프리팹
-    [Header("스폰할 적 프리팹")][SerializeField] private Enemy _enemyPrefab;
+    [SerializeField] private Enemy _enemyPrefab;
 
     private void Update()
     {
@@ -19,6 +19,8 @@ public class EnemySpawner : MonoBehaviour
         if (_timer >= _spawnInterval)
         {
             _timer = 0;
+
+            _spawnInterval = Random.Range(1f, 3f); // float: 1 ~ 3
 
             Spawn();
         }
