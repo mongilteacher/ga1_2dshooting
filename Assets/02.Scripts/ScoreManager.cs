d4247ecf2d9736c45baf3ccd85668c96d12ca898
mongilteacher <mongilteacher@gmail.com>
2026-09-10 14:45:15 +0900

    feat: 최고 점수 저장 / 로드
    - PlayerPrefs 이용

--- Assets/02.Scripts/ScoreManager.cs
+++ Assets/02.Scripts/ScoreManager.cs
@@ -12,7 +12,9 @@ public class ScoreManager : MonoBehaviour
     // 관리: 특정 데이터에 대한 무결성과 생성,읽기,수정,삭제 등과 관련된 게임 로직
     private int _bestScore;
     private int _currentScore = 0;
-    private int _lastRefreshScore = -1;
+
+    // 저장키
+    private const string SaveKey = "BestScore";
 
     // UI 책임 추가 (텍스트메시 프로 참조)
     [SerializeField] private TextMeshProUGUI _bestScoreTextUI;
@@ -31,6 +33,22 @@ public class ScoreManager : MonoBehaviour
         _instance = this;
     }
 
+    private void Start()
+    {
+        // 입력: Input.
+        // 저장/불러오기: PlayerPrefs
+
+        if (PlayerPrefs.HasKey(SaveKey))
+        {
+            _bestScore = PlayerPrefs.GetInt(SaveKey);
+        }
+
+        _bestScore = PlayerPrefs.GetInt(SaveKey, 0);
+
+
+        Refresh();
+    }
+
 
     public void AddScore(int score)
     {
@@ -40,22 +58,19 @@ public class ScoreManager : MonoBehaviour
         if (_currentScore > _bestScore)
         {
             _bestScore = _currentScore;
+
+            // 저장: PlayerPrefs.Set~ 시리즈를 이용해서 int/float/string을 저장 가능하다.
+            // 내 컴퓨터 어딘가에 저장이 된다..
+            PlayerPrefs.SetInt(SaveKey, _bestScore);
+            PlayerPrefs.Save();
         }
-    }
 
-
-    private void Update()
-    {
         Refresh();
     }
 
     private void Refresh()
     {
-        if (_lastRefreshScore == _currentScore) return;
-
         _bestScoreTextUI.text = $"BestScore: {_bestScore}";
         _currentScoreTextUI.text = $"Score: {_currentScore}";
-
-        _lastRefreshScore = _currentScore;
     }
 }
