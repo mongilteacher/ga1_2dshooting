a39a59ee8f3f8c0df3dd075ab538f71ec5356e42
mongilteacher <mongilteacher@gmail.com>
2026-09-10 13:57:36 +0900

    feat: 점수 관리자 하나임을 보장
    - 늦게 태어난 점수 관리자는 Destroy

--- Assets/02.Scripts/ScoreManager.cs
+++ Assets/02.Scripts/ScoreManager.cs
@@ -6,7 +6,8 @@ public class ScoreManager : MonoBehaviour
     // 싱글톤 패턴
     // 1. 전역적으로 접근 가능하다.
     // 2. 인스턴스(생성된 객체)가 하나임을 보장한다.
-    public static ScoreManager Instance;
+    private static ScoreManager _instance = null;
+    public static ScoreManager Instance => _instance;
 
     // 관리: 특정 데이터에 대한 무결성과 생성,읽기,수정,삭제 등과 관련된 게임 로직
     private int _bestScore;
@@ -19,7 +20,14 @@ public class ScoreManager : MonoBehaviour
 
     private void Awake()
     {
-        Instance = this;
+        // 늦게 태어난 매니저는 나는 늦었네~ 하면서 삭제
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
     }
 
 
