075feac0b8ae2f6e6aba34a491bb65838ca871af
mongilteacher <mongilteacher@gmail.com>
2026-09-11 10:18:29 +0900

    feat: 에너미 스폰: 가중치 기반 랜덤 선택
    - EnemySpawnData

--- Assets/02.Scripts/Enemy/EnemySpawner.cs
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -7,10 +7,10 @@ public class EnemySpawner : MonoBehaviour
     // - 타이머
     [SerializeField] private float _spawnInterval = 3f;
 
+    [SerializeField] private EnemySpawnData[] _spawnDatas;
+
     private float _timer;
 
-    // - 생성할 프리팹들
-    [SerializeField] private Enemy[] _enemyPrefabs;
 
     private void Update()
     {
@@ -33,27 +33,35 @@ public class EnemySpawner : MonoBehaviour
         // 30%: [1] Aimed
         // 20%: [2] Homing
 
-        int enemyPrefabIndex = 0;
-        int radomPercent = UnityEngine.Random.Range(0, 100);
-
         // Todo: Scriptable Object를 사용해서 리팩토링
         // 이유 1: 배열을 사용했지만 각 아이템이 어떤 프리팹인지 알수가 없음
         // 이유 2: 각 에너미 스폰 확률을 매직 넘버로 하드코딩해서 유지보수가 어렵
-        if (radomPercent < 50)
-        {
-            enemyPrefabIndex = 0;
-        }
-        else if (radomPercent < 80)
-        {
-            enemyPrefabIndex = 1;
-        }
-        else
-        {
-            enemyPrefabIndex = 2;
-        }
 
 
-        Enemy enemy = Instantiate(_enemyPrefabs[enemyPrefabIndex]);
-        enemy.transform.position = transform.position;
+        // 가중치 랜덤 선택
+        // 각 아이템에 가중치를 부여하고, 가중치가 클수록 높은 확률로 선택되도록 하는 방식
+
+        // 1. 추첨할 수 있는 모든 가중치를 더한다.
+        int totalWeight = 0;
+        foreach (EnemySpawnData data in _spawnDatas)
+        {
+            totalWeight += data.Weight;
+        }
+
+        // 2. 전체 가중치 범위에서 랜덤한 정수를 뽑는다.
+        int randomWeight = Random.Range(0, totalWeight);
+
+        // 3. 가중치를 누적하면서 선택된 구간을 찾는다.
+        int cumulativeWeight = 0;
+        foreach (EnemySpawnData data in _spawnDatas)
+        {
+            cumulativeWeight += data.Weight; // 누적
+            if (randomWeight < cumulativeWeight) // 구간
+            {
+                GameObject enemy = Instantiate(data.EnemyPrefab);
+                enemy.transform.position = transform.position;
+                break;
+            }
+        }
     }
 }
