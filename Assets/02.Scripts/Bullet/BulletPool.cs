205aeebcd5f477ae7cadad11065720290eae7e6d
mongilteacher <mongilteacher@gmail.com>
2026-09-11 15:01:44 +0900

    fix: 2차원 배열 방식으로 풀링 수정
    feat: 오랜만에 2차원 배열로 풀링을 만들어서 힘들었다.

--- Assets/02.Scripts/Bullet/BulletPool.cs
+++ Assets/02.Scripts/Bullet/BulletPool.cs
@@ -15,7 +15,7 @@ public class BulletPool : MonoBehaviour
     [SerializeField] private int _poolSize;
 
     // 생성한 촐알을 담아둘 풀
-    private Bullet[] _pool;
+    private Bullet[,] _pool;
 
     private static BulletPool _instance = null;
     public static BulletPool Instance => _instance;
@@ -31,37 +31,46 @@ public class BulletPool : MonoBehaviour
         _instance = this;
 
         // 창고를 창고 크기만큼 만든다.
-        _pool = new Bullet[_poolSize * _bulletPrefabs.Length];
+        _pool = new Bullet[_bulletPrefabs.Length, _poolSize];
 
         // 총알 프리팹 종류와 창고 크기 만큼 총알을 미리 만들어서 집어 넣는다.
-        foreach (Bullet bulletPrefab in _bulletPrefabs) // [메인 총알 프리팹, 서브 총알 프리팹]
+        for (int i = 0; i < _bulletPrefabs.Length; i++)
         {
-            for (int i = 0; i < _poolSize; i++)
+            Bullet bulletPrefab = _bulletPrefabs[i]; // [메인 총알 프리팹, 서브 총알 프리팹]
             {
-                Bullet bullet = Instantiate(bulletPrefab, gameObject.transform);
-                bullet.gameObject.SetActive(false); // 당장 사용할거 아니기에 비활성화
-                _pool[i] = bullet;
+                for (int j = 0; j < _poolSize; j++)
+                {
+                    Bullet bullet = Instantiate(bulletPrefab, gameObject.transform);
+                    bullet.gameObject.SetActive(false); // 당장 사용할거 아니기에 비활성화
+                    _pool[i, j] = bullet;
+                }
             }
         }
     }
 
     public Bullet GetBullet(BulletType bulletType)
     {
-        foreach (Bullet bullet in _pool)
+        for (int i = 0; i < _pool.Length; i++) // 타입별로 순회 하면서
         {
-            if (bullet.Type != bulletType)
+            if (_pool[i, 0].Type != bulletType) // 첫번째 요소의 타입이 내가 원하는게 아니라면 스킵
             {
                 continue;
             }
 
-            // 비활성화 되어있는 (즉, 누가 빌려가지 않은 ) 총알 반환
-            if (bullet.gameObject.activeSelf == false)
+            for (int j = 0; j < _poolSize; j++) // 원하는 타입의 배열 순회
             {
-                bullet.gameObject.SetActive(true);
-                bullet.OnSpawn();
-                return bullet;
+                Bullet bullet = _pool[i, j];
+
+                // 비활성화 되어있는 (즉, 누가 빌려가지 않은 ) 총알 반환
+                if (bullet.gameObject.activeSelf == false)
+                {
+                    bullet.gameObject.SetActive(true);
+                    bullet.OnSpawn();
+                    return bullet;
+                }
             }
         }
+
 
         return null;
     }
